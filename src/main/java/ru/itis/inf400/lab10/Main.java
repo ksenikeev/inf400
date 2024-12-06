0b5aca1cebb24c7803d9148ca0121e02ed41d5fc
ksenikeev <31629851+ksenikeev@users.noreply.github.com>
2024-12-06 15:12:21 +0300

    Update Main.java

--- src/main/java/ru/itis/inf400/lab10/Main.java
+++ src/main/java/ru/itis/inf400/lab10/Main.java
@@ -2,6 +2,6 @@ package ru.itis.inf400.lab10;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 11-400!");
+        System.out.println("Hello 11-400!!!!");
     }
 }
