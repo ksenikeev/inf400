dd14edb4ec515e7600ad46761b68dd54f99e7521
ksenikeev <ksenikeev@mail.ru>
2024-12-13 15:07:24 +0300

    game

--- lab11_game/src/main/java/ru/itis/inf400/lab11/game/Game.java
+++ lab11_game/src/main/java/ru/itis/inf400/lab11/game/Game.java
@@ -11,6 +11,8 @@ public class Game {
     private int zoneEndX;
     private int zoneEndY;
     private Player winner;
+    private Player[] players;
+    private int steps = 0;
 
     public Game(){}
 
@@ -22,11 +24,35 @@ public class Game {
         Player.setFieldSize(fieldWidth,fieldHeight);
         Random random = new Random();
         Ant ant = new Ant(random.nextInt(fieldWidth),random.nextInt(fieldHeight));
-
+        players = new Player[] {ant};
 
     }
 
-    public
+    public void start() {
+        steps = 1;
+        while (true) {
+            for (Player player: players) {
+                player.move();
+                if (zoneStartX <= player.getPositionX() &&
+                        player.getPositionX() <= zoneEndX &&
+                        zoneStartY <= player.getPositionY() &&
+                        player.getPositionY() <= zoneEndY) {
+                    winner = player;
+                    return;
+                }
+            }
+
+            steps++;
+        }
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
 
 
 }
