ec4411aef25c0892c0e14b2375ea3bd43f5b47db
ksenikeev <ksenikeev@mail.ru>
2024-12-13 14:18:55 +0300

    game

---
+++ lab11_game/src/main/java/ru/itis/inf400/lab11/model/Player.java
@@ -0,0 +1,49 @@
+package ru.itis.inf400.lab11.model;
+
+public class Player {
+    protected String name;
+    protected int positionX;
+    protected int positionY;
+
+    public void move(){
+
+    }
+
+    public Player() {
+
+    }
+
+    public Player(String name, int positionX, int positionY) {
+        this.name = name;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", " + "Position: (" + positionX + ", " + positionY + ")";
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
