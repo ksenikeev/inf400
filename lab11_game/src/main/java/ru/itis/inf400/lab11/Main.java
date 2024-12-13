6577604fd454ff646a7d917a4393af71135c9554
ksenikeev <ksenikeev@mail.ru>
2024-12-13 15:14:47 +0300

    game

--- lab11_game/src/main/java/ru/itis/inf400/lab11/Main.java
+++ lab11_game/src/main/java/ru/itis/inf400/lab11/Main.java
@@ -1,5 +1,15 @@
 package ru.itis.inf400.lab11;
 
-public class Main {
+import ru.itis.inf400.lab11.game.Game;
+import ru.itis.inf400.lab11.model.Player;
 
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(1000, 1000,
+                450, 450, 500, 500);
+        game.start();
+        Player winner = game.getWinner();
+        int steps = game.getSteps();
+        System.out.println(winner + " " + steps);
+    }
 }
