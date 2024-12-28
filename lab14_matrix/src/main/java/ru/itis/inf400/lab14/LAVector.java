a74bf494c53a9d5f12eba0e04e5c739c0510652b
ksenikeev <ksenikeev@mail.ru>
2024-12-28 10:26:09 +0300

    Функциональный интерфейс

---
+++ lab14_matrix/src/main/java/ru/itis/inf400/lab14/LAVector.java
@@ -0,0 +1,25 @@
+package ru.itis.inf400.lab14;
+
+public class LAVector {
+    private float[] value;
+
+    public LAVector(int size) {
+        //TODO заполнить нулями
+    }
+
+    public LAVector(int size, float value) {
+        //TODO заполнить значением value
+    }
+
+    public LAVector(float[] value) {
+
+    }
+
+    public float[] getComponents() {
+        return null;
+    }
+
+    public float getComponent(int index) {
+        return 0;
+    }
+}
