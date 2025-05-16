ab444dfd6267de413dd47710aed6b2aea2c698a1
ksenikeev <ksenikeev@mail.ru>
2025-05-16 14:44:04 +0300

    serialization

--- lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/Main.java
+++ lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/Main.java
@@ -1,7 +1,10 @@
 package ru.itis.inf400.lab2_11;
 
+import ru.itis.inf400.lab2_11.model.Booking;
+
 public class Main {
     public static void main(String[] args) {
-
+        Booking booking = new Booking();
+        booking.setId(1);
     }
 }
--- lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/model/Booking.java
+++ lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/model/Booking.java
@@ -1,7 +1,11 @@
 package ru.itis.inf400.lab2_11.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter@Setter
 public class Booking {
     private Integer id;
     private Date arrivaldate;
@@ -9,52 +13,4 @@ public class Booking {
     private Date departuredate;
     private Hotel hotel;
     private Person person;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getArrivaldate() {
-        return arrivaldate;
-    }
-
-    public void setArrivaldate(Date arrivaldate) {
-        this.arrivaldate = arrivaldate;
-    }
-
-    public Date getStayingdate() {
-        return stayingdate;
-    }
-
-    public void setStayingdate(Date stayingdate) {
-        this.stayingdate = stayingdate;
-    }
-
-    public Date getDeparturedate() {
-        return departuredate;
-    }
-
-    public void setDeparturedate(Date departuredate) {
-        this.departuredate = departuredate;
-    }
-
-    public Hotel getHotel() {
-        return hotel;
-    }
-
-    public void setHotel(Hotel hotel) {
-        this.hotel = hotel;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
 }
