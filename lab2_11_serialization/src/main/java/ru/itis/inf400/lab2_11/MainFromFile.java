e0291661cf24e652f65b9fc356b80e52a06e1e46
ksenikeev <ksenikeev@mail.ru>
2025-05-16 16:09:54 +0300

    serialization

--- lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/MainFromFile.java
+++ lab2_11_serialization/src/main/java/ru/itis/inf400/lab2_11/MainFromFile.java
@@ -1,7 +1,21 @@
 package ru.itis.inf400.lab2_11;
 
+import ru.itis.inf400.lab2_11.model.Booking;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class MainFromFile {
     public static void main(String[] args) {
+        try (ObjectInputStream ois
+                = new ObjectInputStream(new FileInputStream("booking.obj"))) {
 
+            Booking booking = (Booking) ois.readObject();
+
+            System.out.println(booking);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
