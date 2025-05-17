99375e256211f96dcbaf86a08685dd80d91a149b
ksenikeev <ksenikeev@mail.ru>
2025-05-17 11:50:28 +0300

    db

--- lab2_12_db/src/main/java/ru/itis/inf400/lab2_12/db/DBUtils.java
+++ lab2_12_db/src/main/java/ru/itis/inf400/lab2_12/db/DBUtils.java
@@ -2,10 +2,7 @@ package ru.itis.inf400.lab2_12.db;
 
 import ru.itis.inf400.lab2_12.model.Book;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
@@ -24,17 +21,17 @@ public class DBUtils {
         }
 
         try(ByteArrayOutputStream data = new ByteArrayOutputStream();
-                DataOutputStream dos = new DataOutputStream(data)) {
+                DataOutputStream dos = new DataOutputStream(data);
+                // открываем файловый поток в режиме добавления в конец
+                FileOutputStream fos = new FileOutputStream(file, true)) {
+
             dos.writeInt(book.getId()); // id
             dos.writeByte(1); // flag
             dos.writeInt(objectData.length); // size
             dos.write(objectData); // object
 
             // пишем в конец файла
-            Files.write(
-                    file.toPath(),
-                    data.toByteArray(),
-                    StandardOpenOption.APPEND);
+            fos.write(data.toByteArray());
 
             Index idx = new Index();
             idx.setId(book.getId());
